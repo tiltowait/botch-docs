99ab98ca0137a020c575965527d62debbb8ada0e
tiltowait <208040+tiltowait@users.noreply.github.com>
2025-03-10 23:20:17 -0700

    Enable sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://botch.lol',
   integrations: [
       starlight({
           plugins: [catppuccin()],
